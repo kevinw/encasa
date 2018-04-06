05029c1d24f3814981ec1ffbe2e97557890bb101
Kevin Watters <kevinwatters@gmail.com>
2018-04-05 20:48:15 -0400

    doing some initial data modelling

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,52 @@
-//! A Hello World example application for working with Gotham.
+#[macro_use]
+extern crate serde_derive;
 
 extern crate futures;
 extern crate gotham;
 extern crate hyper;
 extern crate mime;
+extern crate serde;
+extern crate serde_json;
 
 use hyper::{Response, StatusCode};
 
 use gotham::http::response::create_response;
 use gotham::state::State;
+
+use std::time::{Duration, SystemTime};
+
+#[derive(Serialize, Deserialize, Debug)]
+struct RecordFileModificationTime
+{
+    modification_time: SystemTime
+}
+
+fn get_dummy_data() -> Task
+{
+    let one_week_seconds = 60 * 60 * 24 * 7;
+
+    let task = Task {
+        name: String::from("Journal"),
+        frequency_goal: Duration::new(one_week_seconds, 0),
+        records: Vec::new(),
+    };
+
+    task
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Record
+{
+    time: SystemTime,
+    note: String,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Task {
+    name: String,
+    frequency_goal: Duration,
+    records: Vec<Record>,
+}
 
 /// Create a `Handler` which is invoked when responding to a `Request`.
 ///
@@ -16,10 +54,12 @@ use gotham::state::State;
 /// We've simply implemented the `Handler` trait, for functions that match the signature used here,
 /// within Gotham itself.
 pub fn say_hello(state: State) -> (State, Response) {
+    let task = get_dummy_data();
+    let serialized = serde_json::to_string(&task).unwrap();
     let res = create_response(
         &state,
         StatusCode::Ok,
-        Some((String::from("Hello World!").into_bytes(), mime::TEXT_PLAIN)),
+        Some((serialized.into_bytes(), mime::TEXT_PLAIN)),
     );
 
     (state, res)
