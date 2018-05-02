ab878c0a3f2d966408c58e871e09cf29d9b9504f
Kevin Watters <kevinwatters@gmail.com>
2018-05-01 20:24:23 -0400

    Add editlocal script

---
+++ tools/editlocal.py
@@ -0,0 +1,12 @@
+import subprocess
+import sys
+import urllib
+
+if __name__ == "__main__":
+    prefix = "editlocal://"
+    if not sys.argv[1].startswith(prefix):
+        raw_input("expected url to start with editlocal://")
+    else:
+        arg = sys.argv[1][len(prefix):]
+        arg = urllib.unquote(arg)
+        subprocess.Popen(["c:\\Program Files (x86)\\Vim\\vim80\\gvim.exe", arg])
